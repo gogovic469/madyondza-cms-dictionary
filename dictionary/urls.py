c70a0632802ab926df5897985d8732286fcdab5d
Musa Baloyi <musabaloyi@aims.ac.za>
2015-04-16 09:25:24 +0530

    Can now display words

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from views import home
+from views import home, all_words
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', home),
     url(r'^home/$', home),
+    url(r'^$', home),
+    url(r'^words/$', all_words),
 )
--- dictionary/views.py
+++ dictionary/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render_to_response
+from django.shortcuts import render
 from models import Entry
 
 def home(request):
     heading = 'Madyondza Dictionary'
     return render_to_response('home.html', {'heading': heading})
+
+def all_words(request):
+    words = Entry.objects.order_by('word')
+    return render(request, 'words.html', {'words': words})
